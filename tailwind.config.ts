e86678190974d04aec44d5a2598447a0b9ccbba9
Eder A. Ramos <79857977+EdyRamos@users.noreply.github.com>
2025-08-09 01:00:58 -0300

    feat: add base theme colors and fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,23 @@
 export default {
   content: ['./index.html', './src/**/*.{ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#4F9DDE',
+        secondary: '#FFD166',
+        success: '#06D6A0',
+        danger: '#EF476F',
+        neutral: {
+          900: '#333333',
+          50: '#FFFFFF',
+        },
+      },
+      fontFamily: {
+        heading: ['"Baloo 2"', 'cursive'],
+        body: ['Inter', 'Nunito', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
+
